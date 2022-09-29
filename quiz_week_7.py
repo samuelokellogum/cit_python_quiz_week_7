469293b65ec51db7d1c5b8a5d73c827203d1e9e3
Samuel Okellogum <oksam21@gmail.com>
2022-09-29 19:54:17 +0300

    add code

--- quiz_week_7.py
+++ quiz_week_7.py
@@ -87,7 +87,7 @@ def return_largest_number():
     list_of_numbers.sort()
     return list_of_numbers[-1]
 
-#4 
+#4 Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
 def cal_upper_and_lower():
     phrase = input("Type in: ")
     phrase = list(phrase)
