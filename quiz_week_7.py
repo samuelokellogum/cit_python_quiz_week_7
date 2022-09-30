30759420c67c597039604e856bc119a51d8b0cfe
Samuel Okellogum <oksam21@gmail.com>
2022-09-30 08:25:52 +0300

    add code

--- quiz_week_7.py
+++ quiz_week_7.py
@@ -1,3 +1,5 @@
+import random
+
 # No 1
 
 class Animal:
@@ -104,7 +106,78 @@ def cal_upper_and_lower():
     print("UPPER:", u)
     print("lower:", l)
 
+#5
+def roll():
+    return random.choice([1,2,3,4,5,6])
 
+class player(object):
+    def __init__(self, name, age, colour):
+        self.name = name
+        self.age = age
+        self.colour = colour
+        
+    def score(self, score):
+        self.score = score    
+    def getscore(self):
+        return self.score
+    
+    def getname(self):
+        return self.name
+    def __str__(self):
+        return 'NAME: ' + self.name + '\nCOLOUR: ' + self.colour + '\nSCORE: ' + str(self.score)
+    
+
+class game(object):
+    def __init__(self, playr, trails):
+        self.trails = trails
+        self.playr = playr
+        
+    def gaming(self):
+        throw = 0
+        score = 0
+        for i in range(self.trails):
+            throw = roll()
+            if throw == 6:
+                throw = throw + roll()
+            score = throw + score    
+        return score
+        
+    def __str__(self):
+        return self.playr.getname() + str(self.score)
+tri = 123        
+    
+
+
+david = player('david', 24, 'green')
+collins = player('collins', 25, 'yellow')
+levi = player('levi', 14, 'red')
+davin = player('davin', 13, 'blue')
+print("-----------Playing Dice Game--------------\n" )
+
+davidscr = game(david, tri)
+collinsscr = game(collins, tri)
+leviscr = game(levi, tri)
+davinscr = game(davin, tri)
+
+scr = []
+scr.append(zackscr.gaming())
+scr.append(johnyscr.gaming())
+scr.append(kinascr.gaming())
+scr.append(usherscr.gaming())
+
+scrsort = sorted(scr)
+for el in scrsort:
+    print(el)
+
+david.score(scr[0])
+collins.score(scr[3])
+levi.score(scr[2])
+davin.score(scr[1])
+
+print(david, '\n')
+print(collins, '\n')
+print(levi, '\n')
+print(davin, '\n')
 
 #6 Write a Python program that lists out all the default as well as custom properties of the class.
 
